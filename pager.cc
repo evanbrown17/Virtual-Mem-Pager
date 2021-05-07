b9a131eace917fec12baffaad18b1b26b0b38ffb
Braden Fisher <bfisher@bowdoin.edu>
2021-05-06 23:40:51 -0400

    Working on syslog

--- pager.cc
+++ pager.cc
@@ -8,11 +8,13 @@
 #include <list>
 #include <map>
 #include <deque>
+#include <string>
 
 
 using namespace std;
 
 struct Page {
+	int page_num;
 	int ref_bit;
 	int dirty_bit;
 	unsigned frame; //physmem location (if any)
@@ -23,26 +25,29 @@ struct Page {
 struct Process {
 	pid_t pid;
 	page_table_t* process_ptbr;
-	page_table_t process_pgtable;
-	uintptr_t validceiling;
-	list<Page*> pageInfo;
-	
-
-	
+	page_table_t* process_pgtable;
+	uintptr_t valid_floor;
+	uintptr_t valid_ceiling;
+	list<Page*> page_info;	
 };
 
 
 list<Process*> all_processes;
 Process* curr_process;
 
+list<Page*> all_pages
+
 unsigned mem_pages;
+unsigned frame_counter;
 unsigned blocks;
-unsigned blocksAssigned;
+unsigned block_counter;
 //unsigned framesAssigned;
 //
 int * framesAssigned;
 
+uintptr_t arena_base;
 
+int total_pages;
 
 /*
  * vm_init
@@ -52,9 +57,12 @@ int * framesAssigned;
  * disk blocks in the raw disk.
  */
 void vm_init(unsigned memory_pages, unsigned disk_blocks) {
+	arena_base = VM_ARENA_BASEADDR;
+	frame_counter = 0;
  	mem_pages = memory_pages;
 	blocks = disk_blocks;
-	blocksAssigned = 0;
+	block_counter = 0;
+	total_pages = 0;
 	framesAssigned = new int [mem_pages]; //says whether the frame is full or not
 	for (unsigned i = 0; i < mem_pages; i++) {
 		framesAssigned[i] = 0;	
@@ -73,8 +81,16 @@ void vm_create(pid_t pid) {
   
 	Process* new_process = new Process;
 	new_process->pid = pid;
-	new_process->validceiling = (uintptr_t)  VM_ARENA_BASEADDR;
+	new_process->valid_floor = VM_ARENA_BASEADDR;
+	new_process->valid_ceiling = VM_ARENA_BASEADDR;
+
+	new_process->process_pgtable = new page_table_t;
+	new_process->process_ptbr = new_process->process_pgtable;
+	new_process->process_pgtable->ptes = new page_table_entry_t;
 	cerr << "New process pid is " << new_process->pid << endl;
+
+	//Need to initialize values in actual page table?
+
 	all_processes.push_back(new_process);
 
 
@@ -96,7 +112,6 @@ void vm_switch(pid_t pid) {
 		}	
 	}
 	cerr << "There is no process with the given pid: " << pid << endl;
-	
 		
 }
 
@@ -136,10 +151,11 @@ void vm_destroy() {
  */
 void* vm_extend() {
 		
-	if ((curr_process->validceiling != (uintptr_t) VM_ARENA_BASEADDR + (uintptr_t)VM_ARENA_SIZE) &&
-			blocksAssigned < blocks){ //check to make sure we haven't run out of arena or disk
-		
-		unsigned nextFrame = mem_pages + 1;
+	if ((curr_process->validceiling != (uintptr_t) VM_ARENA_BASEADDR + (uintptr_t)VM_ARENA_SIZE - 1) &&
+			block_counter < blocks){ //check to make sure we haven't run out of arena or disk
+		/*
+		unsigned nextFrame = frame_counter;
+		frame_counter++;
 
 		//find lowest unused frame
 		for (unsigned i = 0; i < mem_pages; i++) {
@@ -148,41 +164,45 @@ void* vm_extend() {
 				break;
 			}
 		}
+		
 		//if no unused frame, cannot alloc		
 		if (nextFrame > mem_pages) {
 			return nullptr;
 		}
+		*/
 
-		
+		//I think we can just put it on the disk and not in memory when it is created?
 	
 		Page* newPage = new Page;
+		newPage->page_num = VM_ARENA_BASEPAGE + total_pages;
+		total_pages++;
 		newPage->dirty_bit = 0;
-		newPage->ref_bit = 1;
-		newPage->frame = nextFrame;
-		newPage->block = blocksAssigned;
+		newPage->ref_bit = 0;
+		newPage->frame = NULL;
+		newPage->block = block_counter;
 		
+		all_pages.push_back(new_page)
 		curr_process->pageInfo.push_back(newPage);
 
-		blocksAssigned++; //will probably need more complexity here to write over disk entries that are no longer needed
+		block_counter++; //will probably need more complexity here to write over disk entries that are no longer needed
 
+		pgTable_index = curr_process->pageInfo.size() - 1;
+		
+		curr_process->process_pgtable->ptes[pgTable_index] = new page_table_entry_t;
+		curr_process->process_pgtable->ptes[pgTable_index]->ppage = NULL;
+		curr_process->process_pgtable->ptes[pgTable_index]->read_enable = 0;
+		curr_process->process_pgtable->ptes[pgTable_index]->write_enable = 0;
 
-		//set page table entry
-		page_table_base_register->ptes[curr_process->pageInfo.size()].ppage = nextFrame;
-	
-		page_table_base_register->ptes[curr_process->pageInfo.size()].read_enable = 1;
-		page_table_base_register->ptes[curr_process->pageInfo.size()].write_enable = 1; 
+		uintptr_t return_value = VM_ARENA_BASEADDR + (VM_PAGESIZE * (curr_process->pageInfo.size() - 1)); 
 		
-
-		
-			
-		
-		curr_process->validceiling += (uintptr_t) VM_PAGESIZE; //update which virtual addresses are valid
+		curr_process->validceiling = return_value; //update which virtual addresses are valid
 		return (void*) curr_process->validceiling;
 
 	} else {
 		return nullptr;
 	}
 }
+
 
 /*
  * vm_syslog
@@ -192,7 +212,31 @@ void* vm_extend() {
  * address or length is invalid.
  */
 int vm_syslog(void* message, unsigned len) {
-  // TODO
-  return -1;
+
+	//make sure the message address is in the arena
+	if ((uintptr_t) message < VM_ARENA_BASEADDR || (uintptr_t) message > curr_process->validceiling) {
+		return -1;
+	}
+
+	//make sure the message address plus the length is in the arena
+	if ((uintptr_t) message + len > curr_process->validceiling || len == 0) {
+		return - 1;
+	}
+
+	string str;
+	while (str.length() < len) {
+		//find page number of address
+		int page_mask = ~(-1 >> 12);
+		unsigned page_num = (message >> 13) & page_mask;
+
+		if (curr_process->process_pgtable->ptes[curr_page]->read_enable == 0) {
+			vm_fault(message, false);
+		}
+
+		//add to string
+	}
+
+	return 0;
+
 }
 
