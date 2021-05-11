d05d2d8e56cbbca5931f6dbafc647bc21366468f
Evan Brown <ebrown3@bowdoin.edu>
2021-05-11 17:15:33 -0400

    My test cases

---
+++ nonrezsyslog.4.cc
@@ -0,0 +1,20 @@
+// sample.cc - a sample application program that uses the external pager
+
+#include "vm_app.h"
+
+int main() {
+    char* p;
+    p = (char*) vm_extend(); // p is an address in the arena
+   
+    p[0] = 'h';
+    p[1] = 'e';
+    p[2] = 'l';
+    p[3] = 'l';
+    p[4] = 'o';
+	
+     p += 10000;
+
+    vm_syslog(p, 5); // pager logs "hello"
+    return 0;
+}
+
